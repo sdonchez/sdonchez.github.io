a4edda8688c8d55bc8778a555458e0cc48067655
Stephen Donchez <sdonchez@villanova.edu>
2020-05-11 12:51:43 -0400

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,26 +1,26 @@
 <template>
 <div>
-	<b-navbar sticky="bottom" class="navbar-dark bg-secondary px-5">
-		<b-row>
-				Copyright &copy; 2020&mdash;{{ currentYear }}
-				<div class="d-none d-md-block px-1"> Stephen Donchez </div>
-				<div class="icon">
+	<b-row class="bg-secondary px-5 py-3">
+		<b-col lg=6>
+			Copyright &copy; 2020&mdash;{{ currentYear }}
+			<div class="d-none d-md-inline px-1"> Stephen Donchez </div>
+			<div class="icon">
 				<a href="mailto:sdonchez@villanova.edu" v-b-tooltip title="Email">
 					<font-awesome-icon icon="envelope" :style="{color: 'white' }"/>
 				</a>
-				</div>
-				<div class="icon">
+			</div>
+			<div class="icon">
 				<a href="https://github.com/sdonchez" v-b-tooltip title="GitHub">
 					<font-awesome-icon :icon="['fab', 'github']" :style="{color: 'white' }"/>
 				</a>
-				</div>
-				<div class="icon">
+			</div>
+			<div class="icon">
 				<a href="https://www.linkedin.com/in/stephen-donchez/" v-b-tooltip title="LinkedIn">
 					<font-awesome-icon :icon="['fab', 'linkedin']" :style="{color: 'white' }"/>
 				</a>
-				</div>
-		</b-row>
-	</b-navbar>
+			</div>
+		</b-col>
+	</b-row>
 </div>
 </template>
 
@@ -37,6 +37,7 @@ export default {
 
 <style>
 .icon{
+	display:inline;
 	padding-left: 0.5rem;
 }
 </style>
