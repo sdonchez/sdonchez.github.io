5f0936d26fcb8ee5295d80c82ffd8360ae33dfd0
Stephen Donchez <sdonchez@villanova.edu>
2023-02-20 19:30:29 -0500

    Post-Grad update 3

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -17,12 +17,7 @@
 					On paper, I'm a Systems Engineer with a Bachelors and a Masters in Computer Engineering and a minor in Computer Science. But that's just a small part of who I am. Sure, I'm passionate about embedded systems, and am excited to be putting my knowledge of software development, FPGAs, and the latest in embedded security to work. But I'm also a 4th degree black belt, have 6 years of experience as a live event technician working in Audio, Video, and Lighting for events of all sizes, and an avid ball-thrower whenever my dog asks. I encourage you to take a moment to explore who I am (in digital form), and to reach out with any questions!
 				</p>
 				<p>
-					You can also download my
-					<a href="SDonchezResumeJune2022.pdf" target="_blank"> resume</a> or visit me on
-					<a href="https://www.linkedin.com/in/stephen-donchez/" target="_blank">
-						LinkedIn</a
-					>
-					to learn more about my professional and academic activities.
+					You can also download my <a href="SDonchezResumeJune2022.pdf" target="_blank"> resume</a> or visit me on	<a href="https://www.linkedin.com/in/stephen-donchez/" target="_blank"> LinkedIn</a> to learn more about my professional and academic activities.
 				</p>
 			</b-row>
 		</b-row>
