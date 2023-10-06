66df80206aeabcc04c4a468230af6e94ad0196c3
Stephen Donchez <sdonchez@villanova.edu>
2023-10-06 17:07:13 -0400

    text cleanup and Alt image tags

--- src/views/Education.vue
+++ src/views/Education.vue
@@ -22,11 +22,11 @@ export default {
 					subtitle2: "Master of Science, Computer Engineering (2020-2022)",
           clug: "VU",
 					text: [
-						"I earned my Bachelor of Science in Computer Engineering from Villanova	University's College of Engineering between August 2016 and May of 2020. During this time, I also completed a minor in Computer Science, and	worked on <a href='#/projects'>a number of interesting projects</a>.",
+						"I earned my Bachelor of Science in Computer Engineering from Villanova University's College of Engineering between August 2016 and May of 2020. During this time, I also completed a minor in Computer Science, and worked on <a href='#/projects'>a number of interesting projects</a>.",
 
-						"I then continued on to earn my Master of Science in Computer Engineering at Villanova, recently completing this degree in Spring of 2022. I researched Embedded Systems design and FPGA security during my	time in this program, with a focus on securing multi-tenant heterogeneous	systems in a cloud environment.",
+						"I then continued on to earn my Master of Science in Computer Engineering at Villanova, recently completing this degree in Spring of 2022. I researched Embedded Systems design and FPGA security during my time in this program, with a focus on securing multi-tenant heterogeneous systems in a cloud environment.",
 
-						"Outside my curricular activities at Villanova, I was heavily	involved in Villanova University Tech Crew, an on campus event production	company. Additionally, I <a href='#/work'>worked</a> for Villanova's	Department of Environmental Health and Safety, providing web and asset management, database development and administration, and assorted related services.",
+						"Outside my curricular activities at Villanova, I was heavily involved in Villanova University Tech Crew, an on campus event production company. Additionally, I <a href='#/work'>worked</a> for Villanova's Department of Environmental Health and Safety, providing web and asset management, database development and administration, and assorted related services.",
 					],
           actions: [
             {
@@ -39,17 +39,18 @@ export default {
           /*noinspect*/
 					icon: "VUENG.jpg",
 					iconRight: "false",
+					alt: "Villanova College of Engineering Seal"
 				},
 				{
 					title: "Lehigh Valley Academy",
 					subtitle1: "High School (2012-2016)",
           slug: "HKA",
 					text: [
-						"From 2012 to 2016, I attended Lehigh Valley Academy Regional Charter	School for my high school degree. LVA is actually a K-12 Charter School, and I attended there for all 13 years of my primary and secondary education.",
+						"From 2012 to 2016, I attended Lehigh Valley Academy Regional Charter School for my high school degree. LVA is actually a K-12 Charter School, and I attended there for all 13 years of my primary and secondary education.",
 
-						"In addition to my traditional high school diploma, LVA afforded me the	opportunity to receive an International Baccalaureate Diploma through the	<a href='https://ibo.org/programmes/diploma-programme/' target='_blank'>IB Diploma Programme</a>. This comprehensive program stressed a balance of liberal arts and STEM	courses, while also putting an emphasis on community service and	individual research.",
+						"In addition to my traditional high school diploma, LVA afforded me the opportunity to receive an International Baccalaureate Diploma through the <a href='https://ibo.org/programmes/diploma-programme/' target='_blank'>IB Diploma Programme</a>. This comprehensive program stressed a balance of liberal arts and STEM courses, while also putting an emphasis on community service and individual research.",
 
-						"Outside of class, a large portion of my time in High School at LVA was	occupied by a club I was heavily invested in, the	<a href='https://tsaweb.org/' target='_blank'>Technology Student Association.</a>As a member of TSA, I worked to create unique physical products, as well	as develop my problem solving skills. I also served as the Pennsylvania	State Historian for the organization. This position required me to produce	all multimedia content for the statewide organization, as well as work alongside the other 7 state officers to plan and execute our 2,000 person	state conference.",
+						"Outside of class, a large portion of my time in High School at LVA was occupied by a club I was heavily invested in, the <a href='https://tsaweb.org/' target='_blank'>Technology Student Association.</a>As a member of TSA, I worked to create unique physical products, as well as develop my problem solving skills. I also served as the Pennsylvania State Historian for the organization. This position required me to produce all multimedia content for the statewide organization, as well as work alongside the other 7 state officers to plan and execute our 2,000 person state conference.",
 					],
           actions:
           [
@@ -60,6 +61,7 @@ export default {
           ],
 					icon: "LVAJaguar.png",
 					iconRight: "2",
+					alt: "Lehigh Valley Academy Logo"
 				},
 			],
 		}
--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -7,86 +7,91 @@
 </template>
 <!--suppress SpellCheckingInspection -->
 <script>
-import InfoCard from "@/components/InfoCard"
-export default {
-	name: "Projects",
-	components: {
-		InfoCard,
-	},
-	data: () => {
-		return {
-			cards: [
-        {
-          title: "An Efficient and Secure Architecture for FPGA-based Multi-Tenant Cloud Applications",
-          subtitle1:"Master's Research",
-          slug: "Masters",
-          text:[
-              "I recently completed my thesis, which focused on enabling the use of FPGAs in a cloud environment. The work is still undergoing review prior to publication and therefore the body of my thesis is not available in the public domain at this time, but the abstract can be found on my <a href=\"/publications\">publications page</a>. If this work intrests you, I'm happy to discuss it and/or see about making the full text available to you. Please don't hesitate to contact me if that's the case.",
-          ],
-          icon:"ConceptDiagramSimplified.png",
-          avatarSquare: true
-        },
-				{
-					title: "ChemCAT",
-					subtitle1: "Villanova University Chemical Container and Asset Tracker",
-          slug: "ChemCAT",
-					text: [
-						"Villanova's Chemical Container and Asset Tracker (ChemCAT) system is	a recent project to provide comprehensive cradle-to-grave tracking of the	university's chemical assets. The project maintains a history of every chemical asset's movement throughout the university from its procurement through its distribution, use, and disposal. It also provides the capacity to look up the Safety Data Sheet (SDS) information associated with each chemical, as well as the	ability to generate reports on individual assets or aggregate data.",
+	import InfoCard from "@/components/InfoCard"
+	export default {
+		name: "Projects",
+		components: {
+			InfoCard,
+		},
+		data: () => {
+			return {
+				cards: [
+					{
+						title: "An Efficient and Secure Architecture for FPGA-based Multi-Tenant Cloud Applications",
+						subtitle1:"Master's Research",
+						slug: "Masters",
+						text: [
+							"I recently completed my thesis, which focused on enabling the use of FPGAs in a cloud environment. The work is still undergoing review prior to publication and therefore the body of my thesis is not available in the public domain at this time, but the abstract can be found on my <a href=\"/publications\">publications page</a>. If this work intrests you, I'm happy to discuss it and/or see about making the full text available to you. Please don't hesitate to contact me if that's the case.",
+						],
+						icon:"ConceptDiagramSimplified.png",
+						avatarSquare: true,
+						Alt: "Concept Diagram depicting the architecture presented in the Thesis"
+					},
+					{
+						title: "ChemCAT",
+						subtitle1: "Villanova University Chemical Container and Asset Tracker",
+						slug: "ChemCAT",
+						text: [
+							"Villanova's Chemical Container and Asset Tracker (ChemCAT) system is a recent project to provide comprehensive cradle-to-grave tracking of the university's chemical assets. The project maintains a history of every chemical asset's movement throughout the university from its procurement through its distribution, use, and disposal. It also provides the capacity to look up the Safety Data Sheet (SDS) information associated with each chemical, as well as the ability to generate reports on individual assets or aggregate data.",
 
-						"The system is built utilizing the Laravel PHP framework to provide a easily navigable user interface. This interface interacts with a MySQL database, via	Laravel's PDO driver. It also harnesses the power of AJAX to facilitate the	dynamic loading of content. The project was developed following a substantial	design process, including the generation of a highly detailed specification and preliminary UI design.",
+							"The system is built utilizing the Laravel PHP framework to provide a easily navigable user interface. This interface interacts with a MySQL database, via Laravel's PDO driver. It also harnesses the power of AJAX to facilitate the dynamic loading of content. The project was developed following a substantial design process, including the generation of a highly detailed specification and preliminary UI design.",
 
-						"Unfortunately, the project's codebase is owned by the university and not publicly available. However, I will gladly answer any questions regarding the details of its implementation.",
-					],
-					icon: "ChemCat.png",
-					avatarSquare: true,
-				},
-				{
-					title: "VUTC Website Redesign",
-					subtitle1: "Villanova University Tech Crew",
-          slug: "VUTC-site",
-					text: [
-						"Beginning in the spring of 2020, I, alongside a group of like minded students,	began a redesign of VUTC's website, through which all aspects of the	organization's operations are managed. The current site (as the redesign is still in progress following several periods of paused work) was developed in PHP without the use of any framework or	cohesive style, and the effort of maintaining it was beginning to exceed the	effort involved in re-writing it from the ground up.",
+							"Unfortunately, the project's codebase is owned by the university and not publicly available. However, I will gladly answer any questions regarding the details of its implementation.",
+						],
+						icon: "ChemCat.png",
+						avatarSquare: true,
+						Alt: "Screenshot of the ChemCAT Dashboard"
+					},
+					{
+						title: "VUTC Website Redesign",
+						subtitle1: "Villanova University Tech Crew",
+						slug: "VUTC-site",
+						text: [
+							"Beginning in the spring of 2020, I, alongside a group of like minded students, began a redesign of VUTC's website, through which all aspects of the organization's operations are managed. The current site (as the redesign is still in progress following several periods of paused work) was developed in PHP without the use of any framework or cohesive style, and the effort of maintaining it was beginning to exceed the effort involved in re-writing it from the ground up.",
 
-						"The new site, like the ChemCAT project above, is built with the Laravel PHP framework and a bootstrapped front-end. The site also utilizes a MySQL/PDO	database system, as well as extensive authentication and authorization gates	to mitigate access to privileged portions of the site.",
-          ],
-					icon: "VUTCWebsite.png",
-					iconRight: "2",
-					avatarSquare: true,
-				},
-				{
-					title: "PrintCheck3D",
-					subtitle1: "Senior Capstone Project",
-          "slug": "PrintCheck3D",
-					text: [
-						"Villanova's ECE capstone project is a three semester, group based experience	that is designed to provide students with experience in the implementation of	a concept under real world circumstances. For our capstone project, my team	worked to implement what became the PrintCheck3D system, a system that monitored the college's fleet of 3D Printers to detect failed printjobs,	and took action on those failures in order to preserve the print where possible, and otherwise to stop the print in order to prevent damage to the printer or	the waste of additional consumable resources.",
+							"The new site, like the ChemCAT project above, is built with the Laravel PHP framework and a bootstrapped front-end. The site also utilizes a MySQL/PDO database system, as well as extensive authentication and authorization gates to mitigate access to privileged portions of the site.",
+						],
+						icon: "VUTCWebsite.png",
+						iconRight: "2",
+						avatarSquare: true,
+						Alt: "Screenshot from the legacy VUTC website"
+					},
+					{
+						title: "PrintCheck3D",
+						subtitle1: "Senior Capstone Project",
+						slug: "PrintCheck3D",
+						text: [
+							"Villanova's ECE capstone project is a three semester, group based experience that is designed to provide students with experience in the implementation of a concept under real world circumstances. For our capstone project, my team worked to implement what became the PrintCheck3D system, a system that monitored the college's fleet of 3D Printers to detect failed printjobs, and took action on those failures in order to preserve the print where possible, and otherwise to stop the print in order to prevent damage to the printer or the waste of additional consumable resources.",
 
-						"The project required, like most embedded systems, a fusion of hardware and	software work. It centered around a Raspberry Pi, which ran a minimized linux	operating system. The project code was authored in C++, and consisted of a number of subsystems responsible for various portions of the system's	operation. The project harnessed a number of publicly available libraries	to perform image processing and hardware interfaces, including the OpenCV library.",
-						"On the hardware side, the project required the use of an infrared camera,	as well as a photogate with custom logic to convert its output to values	appropriate for the Raspberry Pi's GPIO interface. It also required the design and manufacturing of several custom hardware mounts to affix the system components to the printer.",
-					],
-          actions:[
-            {
-              link: "https://github.com/sdonchez/SeniorDesign",
-              text: "Visit the project Github Repo",
-            }
-          ],
-					icon: "PrintCheck3D.jpg",
-					avatarSquare: true,
-				},
-				{
-					title: "HKA Database Management System",
-					subtitle1: "Hoover Karate Academy",
-          slug: "HKADB",
-					text: [
-						"One of my earliest major development projects, the HKA Database Management	System was designed in response to a drastic need for improvement to the	school's pre-existing system for tracking student records. It affords users	a simple, intuitive interface for managing almost all aspects of the school's	operations, including attendance, student progression through the various			ranks, tuition payment, and the generation of assorted administrative	paperwork. It serves to automate a number of process that were previously done	by hand, saving considerable time for the school's administrative staff.",
+							"The project required, like most embedded systems, a fusion of hardware and software work. It centered around a Raspberry Pi, which ran a minimized linux operating system. The project code was authored in C++, and consisted of a number of subsystems responsible for various portions of the system's operation. The project harnessed a number of publicly available libraries to perform image processing and hardware interfaces, including the OpenCV library.",
+							"On the hardware side, the project required the use of an infrared camera, as well as a photogate with custom logic to convert its output to values appropriate for the Raspberry Pi's GPIO interface. It also required the design and manufacturing of several custom hardware mounts to affix the system components to the printer.",
+						],
+						actions:[
+							{
+								link: "https://github.com/sdonchez/SeniorDesign",
+								text: "Visit the project Github Repo",
+							}
+						],
+						icon: "PrintCheck3D.jpg",
+						avatarSquare: true,
+						Alt: "Picture of a 3D printer monitored by PrintCheck3D"
+					},
+					{
+						title: "HKA Database Management System",
+						subtitle1: "Hoover Karate Academy",
+						slug: "HKADB",
+						text: [
+							"One of my earliest major development projects, the HKA Database Management System was designed in response to a drastic need for improvement to the school's pre-existing system for tracking student records. It affords users a simple, intuitive interface for managing almost all aspects of the school's operations, including attendance, student progression through the various ranks, tuition payment, and the generation of assorted administrative paperwork. It serves to automate a number of process that were previously done by hand, saving considerable time for the school's administrative staff.",
 
-						"The system is built on Microsoft Access, with data located in a separate	MySQL database. It implements a custom designed set of authentication	mechanisms (as none were present in Access at the time of development),	and facilitates simultaneous access from both local and remote users to	afford convenience to the school's staff.",
-					],
-					icon: "HKADB.png",
-					iconRight: "2",
-					avatarSquare: true,
-				},
-			],
-		}
-	},
-}
+							"The system is built on Microsoft Access, with data located in a separate MySQL database. It implements a custom designed set of authentication mechanisms (as none were present in Access at the time of development), and facilitates simultaneous access from both local and remote users to afford convenience to the school's staff.",
+						],
+						icon: "HKADB.png",
+						iconRight: "2",
+						avatarSquare: true,
+						Alt: "Screenshot of the main menu of the HKA Database Application"
+					},
+				],
+			}
+		},
+	}
 </script>
