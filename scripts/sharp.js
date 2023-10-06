3b47b1c1a6319fcb85186e0ac988da348efb6296
Stephen Donchez <sdonchez@villanova.edu>
2023-10-06 17:05:24 -0400

    add webp conversion to sharp script

--- scripts/sharp.js
+++ scripts/sharp.js
@@ -2,10 +2,19 @@ const sharp = require('sharp');
 const fs = require('fs');
 const directory = 'src/assets';
 const widths = [128,256,512,1080,1920,3840];
+
 fs.readdirSync(directory).forEach(file => {
-    widths.forEach(width => {
-        sharp(`${directory}/${file}`)
-        .resize(width, null, {withoutEnlargement: true}) // width, height
-        .toFile(`${directory}/${file.split('.').slice(0, -1).join('.')}-${width}.${file.split('.').slice(-1)}`);
-    })
+    if(!file.includes("-"))
+    {
+        widths.forEach(width => {
+            sharp(`${directory}/${file}`)
+            .resize(width, null, {withoutEnlargement: true})
+            .toFile(`${directory}/${file.split('.').slice(0, -1).join('.')}-${width}.${file.split('.').slice(-1)}`);
+
+            sharp(`${directory}/${file}`)
+            .resize(width, null, {withoutEnlargement: true})
+            .webp()
+            .toFile(`${directory}/${file.split('.').slice(0, -1).join('.')}-${width}.webp`);
+        });
+    }
   });
