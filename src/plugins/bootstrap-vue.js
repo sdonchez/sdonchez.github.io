088a5d75679040580c83e32c82a3e9c777939d65
Stephen Donchez <sdonchez@villanova.edu>
2023-10-07 20:42:58 -0400

    Take 3

--- src/plugins/bootstrap-vue.js
+++ src/plugins/bootstrap-vue.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 
 import '../custom.scss'
 import BootstrapVue from 'bootstrap-vue'
-import 'bootswatch/dist/darkly/bootstrap.min.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'bootswatch/dist/darkly/bootstrap.min.css'
 
 Vue.use(BootstrapVue)
