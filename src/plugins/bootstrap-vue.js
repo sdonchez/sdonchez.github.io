203c24dd5e44b90b14756e8d8a1295cd8c9819d7
Stephen Donchez <sdonchez@villanova.edu>
2023-10-07 20:49:52 -0400

    remove custom scss

--- src/plugins/bootstrap-vue.js
+++ src/plugins/bootstrap-vue.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 
-import '../custom.scss'
+//import '../custom.scss'
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import 'bootswatch/dist/darkly/bootstrap.min.css'
