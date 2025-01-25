9f61c71691276977298d753598577d7f9d7ab961
Stephen Donchez <sdonchez@villanova.edu>
2025-01-24 20:55:44 -0500

    Initial update to Vue V3 in compatibility mode

--- src/plugins/bootstrap-vue.js
+++ src/plugins/bootstrap-vue.js
@@ -1,8 +1,6 @@
-import Vue from 'vue'
-
 //import '../custom.scss'
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import 'bootswatch/dist/darkly/bootstrap.min.css'
 
-Vue.use(BootstrapVue)
+export default BootstrapVue; // Export the BootstrapVue3 plugin for use in main.js
--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 // noinspection JSUnusedGlobalSymbols
 module.exports = {
 	chainWebpack: config => {
+		config.resolve.alias.set('vue', '@vue/compat')
 		config.module
 			.rule('vue')
 			.use('vue-loader')
@@ -17,9 +18,22 @@ module.exports = {
 					'b-card-img-lazy': ['src', 'blank-src'],
 					'b-carousel-slide': 'img-src',
 					'b-embed': 'src'
+				},
+
+				options.compilerOptions= {
+					compatConfig: {
+						MODE: 2
+					}
 				}
 
 				return options
+			}),
+			config.plugin('define').tap((definitions) => {
+				Object.assign(definitions[0], {
+					__VUE_OPTIONS_API__: 'true',
+					__VUE_PROD_HYDRATION_MISMATCH_DETAILS__: 'true'
+				})
+				return definitions
 			})
 	},
 		publicPath: "/",
